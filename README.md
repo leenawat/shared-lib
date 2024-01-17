d4841a4619ce907556f275dc829427e07ceecb67
leenawat papahom <leenawat.pa@gmail.com>
2024-01-17 21:41:24 +0700

    finish simple share lib

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # shared-lib
+
+
+https://github.com/tejashreeSalvi/shared-lib/tree/master
+
+
+https://keentolearn.medium.com/how-to-improve-your-jenkins-builds-with-shared-libraries-5e225b7435fb
---
+++ vars/test_pring_msg.groovy
@@ -0,0 +1,7 @@
+def call(Map config){
+	println "++++++++++++++Job Success! Created first shared lib"
+}
+
+def callSecond(Map config){
+	println "++++++++++++++Job Success! Created second shared lib"
+}
