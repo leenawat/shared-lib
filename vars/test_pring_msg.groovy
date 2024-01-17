3bb506baf0d78a0d7d9972d3206dc8ff87234b58
leenawat papahom <leenawat.pa@gmail.com>
2024-01-17 21:55:25 +0700

    checkoutCode update

--- vars/test_pring_msg.groovy
+++ vars/test_pring_msg.groovy
@@ -1,7 +1,9 @@
 def call(Map config){
 	println "++++++++++++++Job Success! Created first shared lib"
+  return true
 }
 
 def callSecond(Map config){
 	println "++++++++++++++Job Success! Created second shared lib"
+  return true
 }
