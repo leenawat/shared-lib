000b785611569de9e8adf8588dcc311ebf7dfc35
leenawat papahom <leenawat.pa@gmail.com>
2024-01-17 22:05:32 +0700

    checkoutCode update

--- vars/checkoutCode.groovy
+++ vars/checkoutCode.groovy
@@ -1,7 +1,7 @@
 def call(String repoUrl, String branch){
 	def workingDir = "${env.WORKSPACE}"
   println "++++++++++++++Job Success! git clone shared lib"
-	sh "git clone ${repoUrl} ${workingDir}"
-        sh "git checkout ${branch}"
+	// sh "git clone ${repoUrl} ${workingDir}"
+        // sh "git checkout ${branch}"
 	return workingDir
 }
